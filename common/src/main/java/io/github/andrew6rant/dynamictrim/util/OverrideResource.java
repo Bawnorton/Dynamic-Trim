a9173d876f7010a46c0d5ca9626575f864732f8a
Benjamin-Norton <bawnorton@me.com>
2023-08-22 21:04:37 -0400

    Improve dynamic resource generation

---
+++ common/src/main/java/io/github/andrew6rant/dynamictrim/util/OverrideResource.java
@@ -0,0 +1,14 @@
+package io.github.andrew6rant.dynamictrim.util;
+
+import com.google.gson.JsonObject;
+import io.github.andrew6rant.dynamictrim.json.JsonHelper;
+import net.minecraft.resource.Resource;
+import net.minecraft.resource.ResourcePack;
+import net.minecraft.util.Identifier;
+import org.apache.commons.io.IOUtils;
+
+public record OverrideResource(Identifier modelId, JsonObject modelResourceJson) {
+    public Resource modelResource(ResourcePack pack) {
+        return new Resource(pack, () -> IOUtils.toInputStream(JsonHelper.toJsonString(modelResourceJson), "UTF-8"));
+    }
+}
